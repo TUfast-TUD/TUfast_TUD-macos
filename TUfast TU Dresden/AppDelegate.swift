565b80748d3125a1bcc2fb0ffccd2ae954608fc6
Julius Schmidt <js+dev@spraken.de>
2020-12-17 23:14:35 +0100

    Initial commit

---
+++ TUfast TU Dresden/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  TUfast TU Dresden
+//
+//  Created by Julius Schmidt on 17.12.20.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ notification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
